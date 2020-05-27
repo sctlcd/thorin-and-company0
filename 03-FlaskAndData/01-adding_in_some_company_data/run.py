08ba49a549f1f6fa11b944bfc12cd0ace89beb61
8 <lucinda.souchet@outlook.com>
2020-05-27 23:11:30 +0100

    add all characters on about page

--- 03-FlaskAndData/01-adding_in_some_company_data/run.py
+++ 03-FlaskAndData/01-adding_in_some_company_data/run.py
@@ -26,5 +26,5 @@ def careers():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=os.environ.get('PORT'),
             debug=True)

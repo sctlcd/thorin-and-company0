b6ff721d9c4e99047d6b794df2d00be6c7aac667
8 <lucinda.souchet@outlook.com>
2020-05-30 01:55:37 +0100

    add 06-using_if_statements_inside_our_html

--- 03-FlaskAndData/05-iterating_over_our_json_data/run.py
+++ 03-FlaskAndData/05-iterating_over_our_json_data/run.py
@@ -1,9 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.secret_key ='some_secret_key'
 
 @app.route('/')
 def index():
@@ -13,13 +13,29 @@ def index():
 @app.route('/about')
 def about():
     data = []
-    with open("03-FlaskAndData/04-reading_from_a_json_file/data/company.json", "r") as json_data:
+    with open("05-iterating_over_our_json_data/data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route('/contact')
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+
+    with open("05-iterating_over_our_json_data/data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
+
+
+@app.route('/contact', methods=["GET","POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form)
+        flash("Thanks {}, we have received your message!".format(request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
